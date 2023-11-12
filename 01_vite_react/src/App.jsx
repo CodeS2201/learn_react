7c9a3dbb93b00b2bd5492903eefb58bedcd6b3f3
CodeS2201 <sourisht22@gmail.com>
2023-11-12 07:57:53 +0530

    used custom reactElement template in 01_vite_react

--- 01_vite_react/src/App.jsx
+++ 01_vite_react/src/App.jsx
@@ -1,10 +1,13 @@
 import Test from './testfile'
 
 function App() {
+  const username = "Learning React on Github";
+
   return (
     <>
       <Test/>
       <h1>Hello to people from github!</h1>
+      <h2>{username}</h2>
     </>
   )
 }
--- 01_vite_react/src/main.jsx
+++ 01_vite_react/src/main.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { createElement } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+  return(
+    <div>
+      <h1>Custom app</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//       href: "google.com",
+//       target: "_blank"
+//   },
+//   children: "click me to visit google"
+// }
+
+const AnotherElement = (
+  <a href='https://google.com' target='_blank'>Visit Google</a>
+)
+
+const AnotherUser = "reactLearner";
+
+const ReactElement = createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'Click me to visit google',
+  AnotherUser
+);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  // AnotherElement
+  ReactElement
 )
